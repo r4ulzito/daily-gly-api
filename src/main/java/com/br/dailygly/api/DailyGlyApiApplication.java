9faa070689ffe57fa58883c51544f6ae71560d92
santos_raul <santos_raul@sicredi.com.br>
2023-09-12 22:28:45 -0300

    estrutura inicial do projeto

---
+++ src/main/java/com/br/dailygly/api/DailyGlyApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.dailygly.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyGlyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyGlyApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/br/dailygly/api/DailyGlyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.dailygly.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyGlyBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
